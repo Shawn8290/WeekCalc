03026709847f9f9e6db2911c2c082d3ae0a4c4e5
Shawn Lu <shawn_lu@gss.com.tw>
2019-11-04 21:40:28 +0800

    ..

--- main.go
+++ main.go
@@ -87,7 +87,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 					diffdays := timeStamp.Sub(lmp).Hours() / 24
 					pWeek := strconv.Itoa(int(diffdays / 7))
 					pDays := strconv.Itoa(int(math.Mod(diffdays, 7)))
-					rtnMsg = timeStamp.String() + "@@" + pWeek + "週" + pDays + "天"
+					rtnMsg = lmp.String() + "@@" + pWeek + "週" + pDays + "天"
 				}				
 				
 				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(rtnMsg)).Do(); err != nil {
